ad351f5037d8e74cfdc9d02c955f60e5908c4de6
soulmachine <soulmachine@gmail.com>
2017-01-25 17:54:09 -0800

    clear content

--- kit6/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router'
-
-export default () =>
-  <div>
-    <Link to="/">Home</Link>
-    {' '}
-    <Link to="counter">Counter</Link>
-  </div>
--- kit7/src/store/configureStore.js
+++
@@ -1,7 +0,0 @@
-// Use ProvidePlugin (Webpack) or loose-envify (Browserify)
-// together with Uglify to strip the dev branch in prod build.
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.production') // eslint-disable-line
-} else {
-  module.exports = require('./configureStore.development') // eslint-disable-line
-}
